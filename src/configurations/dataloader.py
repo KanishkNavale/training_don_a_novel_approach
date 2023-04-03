8f4c90ef45c4f03a9cae73f9019a7f7222d246da
KanishkNavale <navalekanishk@gmail.com>
2023-04-03 18:35:59 +0200

    Adding image and hintergrund probabilities to dataloader configs

--- src/configurations/dataloader.py
+++ src/configurations/dataloader.py
@@ -15,9 +15,23 @@ class DataLoaderConfig:
     n_workers: int
     batch_size: int
 
+    random_hintergrund_probability: float
+    noisy_hintergrund_probability: float
+    masked_hintergrund_probability: float
+
+    greyscale_probability: float
+    colorjitter_probability: float
+    gaussian_blur_probability: float
+
     @classmethod
     def from_dictionary(cls, dictionary: Dict[str, Any]) -> DataLoaderConfig:
         return cls(**dictionary.dataloader)
 
     def __post_init__(self):
         self.directory = os.path.abspath(self.directory)
+
+        if self.random_hintergrund_probability + self.noisy_hintergrund_probability + self.masked_hintergrund_probability != 1.0:
+            raise ValueError("The probabilities of the background augmentation must sum up to 1.")
+
+        if self.greyscale_probability + self.colorjitter_probability + self.gaussian_blur_probability != 1.0:
+            raise ValueError("The probabilities of the image augmentation must sum up to 1.")
